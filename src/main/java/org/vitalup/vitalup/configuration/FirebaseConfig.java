2eebdcb5ac184602d95b9ac90d23d1d9d986ccc5
somiljain2006 <somil16022006@gmail.com>
2026-01-25 00:34:42 +0530

    Removed some warnings

--- src/main/java/org/vitalup/vitalup/configuration/FirebaseConfig.java
+++ src/main/java/org/vitalup/vitalup/configuration/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.InputStream;
@@ -11,16 +12,23 @@ import java.io.InputStream;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${app.firebase-configuration-file}")
+    private String firebaseConfigFile;
+
     @PostConstruct
     public void initialize() {
         try {
             InputStream serviceAccount =
-                    getClass().getClassLoader()
-                            .getResourceAsStream("firebase-service-account.json");
+              getClass().getClassLoader()
+                .getResourceAsStream(firebaseConfigFile);
+
+            if (serviceAccount == null) {
+                throw new IllegalStateException("Firebase config file not found: " + firebaseConfigFile);
+            }
 
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
+              .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+              .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
@@ -30,5 +38,4 @@ public class FirebaseConfig {
             throw new RuntimeException("Firebase initialization failed", e);
         }
     }
-
 }
